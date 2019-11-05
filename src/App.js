03de520dfca2ab7e564616b3917df05b23151814
Daniel Santos <daniels11.dev@gmail.com>
2019-11-05 15:31:10 -0300

    Added Postlist

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Admin, Resource } from 'react-admin';
+import { PostList } from './Posts';
 import { UserList } from './Users';
 import jsonServerProvider from 'ra-data-json-server';
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 const App = () => (
   <Admin dataProvider={dataProvider}>
+      <Resource name="posts" list={PostList} />
       <Resource name="users" list={UserList} />
   </Admin>
 );
